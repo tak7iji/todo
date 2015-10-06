42053cc92e42af2cdf94669f6c90ed8bf9c88c09
E.T <tak7iji@gmail.com>
2015-10-06 17:18:38 +0900

    エラーページ表示に対応

--- without-db-boot/src/main/java/app/AppConfig.java
+++ without-db-boot/src/main/java/app/AppConfig.java
@@ -3,11 +3,15 @@ package app;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.ErrorPage;
 import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.boot.context.embedded.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.filter.DelegatingFilterProxy;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -19,6 +23,22 @@ import org.terasoluna.gfw.web.logging.mdc.XTrackMDCPutFilter;
 @ImportResource({"classpath:META-INF/spring/applicationContext.xml",
     			 "classpath:META-INF/spring/spring-mvc.xml"})
 public class AppConfig extends WebMvcConfigurerAdapter {
+	@Bean
+	public EmbeddedServletContainerCustomizer containerCustomizer(){
+	    return new CustomizerImpl();
+	}
+
+	private static class CustomizerImpl implements EmbeddedServletContainerCustomizer {
+
+	    @Override
+	    public void customize(ConfigurableEmbeddedServletContainer container) {
+	        container.addErrorPages(new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/WEB-INF/views/common/error/systemError.jsp"),
+	                                new ErrorPage(HttpStatus.NOT_FOUND, "/WEB-INF/views/common/error/resourceNotFoundError.jsp"),
+    		                        new ErrorPage(Exception.class, "/WEB-INF/views/common/error/unhandledSystemError.jsp"));
+	    }
+
+	}
+	
 	@Bean
 	public ServletListenerRegistrationBean<HttpSessionEventLoggingListener>  httpSessionEventLoggingListener() {
 		ServletListenerRegistrationBean<HttpSessionEventLoggingListener> listenerRegBean = new ServletListenerRegistrationBean<HttpSessionEventLoggingListener>();
