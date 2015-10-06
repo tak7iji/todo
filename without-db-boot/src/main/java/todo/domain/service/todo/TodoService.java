90d063cc7acf95ae309d478668ed83d2c7926073
E.T <tak7iji@gmail.com>
2015-10-06 16:13:09 +0900

    Serviceインタフェースを追加

--- without-db-boot/src/main/java/todo/domain/service/todo/TodoService.java
+++ without-db-boot/src/main/java/todo/domain/service/todo/TodoService.java
@@ -1,91 +1,18 @@
 package todo.domain.service.todo;
 
-import org.joda.time.DateTime;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import org.terasoluna.gfw.common.date.jodatime.JodaTimeDateFactory;
-import org.terasoluna.gfw.common.exception.BusinessException;
-import org.terasoluna.gfw.common.exception.ResourceNotFoundException;
-import org.terasoluna.gfw.common.message.ResultMessage;
-import org.terasoluna.gfw.common.message.ResultMessages;
 import todo.domain.model.Todo;
-import todo.domain.repository.todo.TodoRepository;
 
-import javax.inject.Inject;
 import java.util.Collection;
-import java.util.Date;
-import java.util.UUID;
 
 /**
  * Created by mash on 2015/10/01.
  */
-@Service("TodoService")
-@Transactional
-public class TodoService {
+public interface TodoService {
+    Collection<Todo> findAll();
 
-	@Value("${max.unfinished.count:5}")
-	private long MAX_UNFINISHED_COUNT;
+    Todo create(Todo todo);
 
-    @Inject
-    TodoRepository todoRepository;
+    Todo finish(String todoId);
 
-    public Todo findOne(String todoId) {
-        Todo todo = todoRepository.findOne(todoId);
-        if (todo == null) {
-
-            ResultMessages messages = ResultMessages.error();
-            messages.add("e.td.sv.4404", todoId);
-            throw new ResourceNotFoundException(messages);
-        }
-        return todo;
-    }
-
-    @Transactional(readOnly = true)
-    public Collection<Todo> findAll() {
-        return todoRepository.findAll();
-    }
-
-    public Todo create(Todo todo) {
-        long unfinishedCount = todoRepository.countByFinished(false);
-        if (unfinishedCount >= MAX_UNFINISHED_COUNT) {
-            ResultMessages messages = ResultMessages.error();
-            messages.add("e.td.sv.2001", MAX_UNFINISHED_COUNT);
-            throw new BusinessException(messages);
-        }
-
-        String todoId = UUID.randomUUID().toString();
-        Date createdAt = new Date();
-
-        todo.setTodoId(todoId);
-        todo.setCreatedAt(createdAt);
-        todo.setFinished(false);
-
-        todoRepository.create(todo);
-        /* REMOVE THIS LINE IF YOU USE JPA
-            todoRepository.save(todo); // 10
-           REMOVE THIS LINE IF YOU USE JPA */
-
-        return todo;
-    }
-
-    public Todo finish(String todoId) {
-        Todo todo = findOne(todoId);
-        if (todo.isFinished()) {
-            ResultMessages messages = ResultMessages.error();
-            messages.add("e.td.sv.2002", todoId);
-            throw new BusinessException(messages);
-        }
-        todo.setFinished(true);
-        todoRepository.update(todo);
-        /* REMOVE THIS LINE IF YOU USE JPA
-            todoRepository.save(todo); // (11)
-           REMOVE THIS LINE IF YOU USE JPA */
-        return todo;
-    }
-
-    public void delete(String todoId) {
-        Todo todo = findOne(todoId);
-        todoRepository.delete(todo);
-    }
+    void delete(String todoId);
 }
