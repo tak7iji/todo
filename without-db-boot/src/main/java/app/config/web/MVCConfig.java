0f2f2670a79e4693f2bc12c760ac26dcc2e91d1a
taka-e <taka-e>
2016-05-25 16:29:24 +0900

    SystemExceptionResolver有効化

--- without-db-boot/src/main/java/app/config/web/MVCConfig.java
+++ without-db-boot/src/main/java/app/config/web/MVCConfig.java
@@ -1,6 +1,7 @@
 package app.config.web;
 
 import java.util.List;
+import java.util.Properties;
 import java.util.regex.Pattern;
 
 import javax.inject.Inject;
@@ -10,32 +11,23 @@ import org.slf4j.LoggerFactory;
 import org.springframework.aop.Advisor;
 import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.beans.factory.config.ConstructorArgumentValues;
-import org.springframework.beans.factory.support.BeanDefinitionRegistry;
-import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
-import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.security.web.method.annotation.AuthenticationPrincipalArgumentResolver;
-import org.springframework.security.web.servlet.support.csrf.CsrfRequestDataValueProcessor;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.support.RequestDataValueProcessor;
+import org.terasoluna.gfw.common.exception.ExceptionCodeResolver;
 import org.terasoluna.gfw.common.exception.ExceptionLogger;
 import org.terasoluna.gfw.web.codelist.CodeListInterceptor;
 import org.terasoluna.gfw.web.exception.HandlerExceptionResolverLoggingInterceptor;
+import org.terasoluna.gfw.web.exception.SystemExceptionResolver;
 import org.terasoluna.gfw.web.logging.TraceLoggingInterceptor;
-import org.terasoluna.gfw.web.mvc.support.CompositeRequestDataValueProcessor;
 import org.terasoluna.gfw.web.token.transaction.TransactionTokenInterceptor;
-import org.terasoluna.gfw.web.token.transaction.TransactionTokenRequestDataValueProcessor;
 
 @Configuration
 @ComponentScan("todo.app")
@@ -112,4 +104,31 @@ public class MVCConfig extends WebMvcConfigurerAdapter {
         return new DefaultPointcutAdvisor(pointcut, handlerExceptionResolverLoggingInterceptor);
 	}
 
+	@Bean(name="systemExceptionResolver")
+	@Inject
+	public SystemExceptionResolver systemExceptionResolver(ExceptionCodeResolver exceptionCodeResolver) {
+		logger.info("Add SystemExceptionResolver");
+		
+		Properties mappings = new Properties();
+		mappings.setProperty("ResourceNotFoundException", "common/error/resourceNotFoundError");
+		mappings.setProperty("BusinessException", "common/error/businessError");
+		mappings.setProperty("InvalidTransactionTokenException", "common/error/transactionTokenError");
+		mappings.setProperty(".DataAccessException", "common/error/dataAccessError");
+		
+		Properties statusCodes = new Properties();
+		statusCodes.setProperty("common/error/resourceNotFoundError", "404");
+		statusCodes.setProperty("common/error/businessError", "409");
+		statusCodes.setProperty("common/error/transactionTokenError", "409");
+		statusCodes.setProperty("common/error/dataAccessError", "500");
+		
+		SystemExceptionResolver systemExceptionResolver = new SystemExceptionResolver();
+		systemExceptionResolver.setExceptionCodeResolver(exceptionCodeResolver);
+		systemExceptionResolver.setExceptionMappings(mappings);
+		systemExceptionResolver.setStatusCodes(statusCodes);
+		systemExceptionResolver.setOrder(3);
+		systemExceptionResolver.setDefaultErrorView("common/error/systemError");
+		systemExceptionResolver.setDefaultStatusCode(500);
+		
+		return systemExceptionResolver;
+	}
 }
