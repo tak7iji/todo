53711fb50ff9c9e1d7f0a68166986ce2b5b31444
taka-e <taka-e>
2016-06-14 16:26:14 +0900

    ログのDB出力対応

--- validation-multi-web/validation-domain/src/main/java/org/sample/validation/common/interceptor/ServiceCallInterceptor.java
+++ validation-multi-web/validation-domain/src/main/java/org/sample/validation/common/interceptor/ServiceCallInterceptor.java
@@ -7,16 +7,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.util.StringUtils;
 import org.terasoluna.gfw.common.exception.ResultMessagesNotificationException;
 
-public class AuditInterceptor implements MethodInterceptor {
+public class ServiceCallInterceptor implements MethodInterceptor {
 
-    private static final Logger logger = LoggerFactory.getLogger(AuditInterceptor.class);
+    private static final Logger logger = LoggerFactory.getLogger(ServiceCallInterceptor.class);
 
     @Override
     public Object invoke(MethodInvocation invocation) throws Throwable {
         String className = invocation.getThis().getClass().getSimpleName();
         String methodName = invocation.getMethod().getName();
         Object[] args = invocation.getArguments();
-        logger.debug("{}[{}] is beeing executed{}", className, methodName,
+        logger.trace("[START SERVICE] {}.{} {}", className, methodName,
                 (args.length > 0) ? " with argument(s): ".concat(StringUtils.arrayToDelimitedString(args, ", ")) : ".");
 
         Object returnObj = null;
@@ -27,11 +27,11 @@ public class AuditInterceptor implements MethodInterceptor {
             // don't output this type exception
             throw re;
         } catch (Throwable t) {
-            logger.debug("Exception occurred in {}[{}].", className, methodName, t);
+            logger.trace("[THROW EXCEPTION] {}.{}", className, methodName, t);
             throw t;
         }
 
-        logger.debug("{}[{}] was executed successfully.", className, methodName);
+        logger.trace("[COMPLETE SERVICE] {}.{}", className, methodName);
         return returnObj;
     }
 }
